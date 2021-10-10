f45cd21b14530bb8fe9d8c075fbfb285bee08f9c
Dermot Clenaghan <dclenaghan@hotmail.com>
2021-10-10 22:42:24 +0000

    update added to form

--- car-insurance/src/components/AdminForm/AdminForm.js
+++ car-insurance/src/components/AdminForm/AdminForm.js
@@ -4,18 +4,31 @@ import { Button, Form, Input } from "semantic-ui-react";
 import SingleDriver from "../SingleDriver/SingleDriver";
 import axios from "axios";
 
-
 function AdminForm() {
-  const [driverId, setDriverId] = useState("");
+  const [driverIdDelete, setDriverIdDelete] = useState("");
+  const [driverIdUpdate, setDriverIdUpdate] = useState("");
+  const [telephoneNumber, setTelephoneNumber] = useState("");
 
   function onDelete(id) {
     const endpointURL = `https://6151d1834a5f22001701d461.mockapi.io/api/v1/people/${id}`;
     axios
-      .delete(endpointURL).then(alert(`Driver ${id} Deleted`)).then(setDriverId(""))
+      .delete(endpointURL)
+      .then(alert(`Driver ${id} Deleted`))
+      .then(setDriverIdDelete(""))
       .catch((err) => {
         console.log(err);
       });
-      
+  }
+
+  function onUpdate(id, phone) {
+    const endpointURL = `https://6151d1834a5f22001701d461.mockapi.io/api/v1/people/${id}`;
+    axios
+      .put(endpointURL, { telephoneNumber: phone })
+      .then(alert(`Driver ${id} Phone Updated to ${telephoneNumber}`))
+      .then(setDriverIdUpdate(""))
+      .catch((err) => {
+        console.log(err);
+      });
   }
 
   return (
@@ -27,11 +40,15 @@ function AdminForm() {
             <label>Delete Driver</label>
             <input
               placeholder="Driver ID"
-              onChange={(e) => setDriverId(e.target.value)}
-              value={driverId}
+              onChange={(e) => setDriverIdDelete(e.target.value)}
+              value={driverIdDelete}
             />
           </Form.Field>
-          <Button color="red" type="submit" onClick={() => onDelete(driverId)}>
+          <Button
+            color="red"
+            type="submit"
+            onClick={() => onDelete(driverIdDelete)}
+          >
             Delete Driver
           </Button>
         </Form.Group>
@@ -40,10 +57,24 @@ function AdminForm() {
         <Form.Group>
           <Form.Field inline>
             <label>Update Phone</label>
-            <input placeholder="Driver ID" />
-            <input placeholder="Phone no." />
+            <input
+              placeholder="Driver ID"
+              onChange={(e) => setDriverIdUpdate(e.target.value)}
+              value={driverIdUpdate}
+
+            />
+            <input
+              placeholder="Phone no."
+              onChange={(e) => setTelephoneNumber(e.target.value)}
+              value={telephoneNumber}
+
+            />
           </Form.Field>
-          <Button color="green" type="submit">
+          <Button
+            color="green"
+            type="submit"
+            onClick={() => onUpdate(driverIdUpdate, telephoneNumber)}
+          >
             Update
           </Button>
         </Form.Group>
